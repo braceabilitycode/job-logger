1960e7a5cae5a20bacf9c0803396da083ebd8078
tonye <tonye@DESKTOP-7P3GIQQ>
2021-03-08 15:26:03 -0600

    add setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='job_logger',
+    version='0.0.1',
+    description='etl job logger',
+    url='https://github.com/braceabilitycode/job-logger.git',
+    author='Braceability',
+    author_email='tony@braceability.com',
+    license='unlicense',
+    packages=['job_logger'],
+    zip_safe=False
+)
